4291a6dff56a10c392804a99edda0b9e38a708de
BurakTetikk <burktetk@gmail.com>
2024-02-11 21:03:37 +0300

    JS

---
+++ Assignments/Part_1/valuesAndVariables/valuesAndVariables.js
@@ -0,0 +1,13 @@
+/*
+1-Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+2-Log their values to the console.
+*/
+
+let country = "Turkey";
+let continent = "Asia";
+let population = "85 millions";
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ script.js
@@ -0,0 +1,2 @@
+alert("JavaScript is FUN!");
+console.log(12 + 43 + 65);
